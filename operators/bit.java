c64cd4c37e9ee112fc35ab52307a7b6999aca35c
DawidSzoka1 <oficer130@gmail.com>
2024-08-29 17:06:12 +0200

    operators

---
+++ operators/bit.java
@@ -0,0 +1,17 @@
+package operators;
+
+public class bit {
+    public static void main(String[] args){
+        /*
+        & - iloczyn bitowy
+        | - suma bitowa
+        ^ - xor
+        x << 1 - przesuniecie w lewo o 1
+        x >> 2 - przesuniecie w prawo o 2
+        ~ negacja bitowa
+         */
+        int a = 6, b = 5;
+        System.out.println(a << 2);
+        System.out.println(a | b);// 011 | 100 = 111
+    }
+}
