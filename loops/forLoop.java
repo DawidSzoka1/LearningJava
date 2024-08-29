dd7fd5ce966c2d75139594c3aff21743517875af
DawidSzoka1 <oficer130@gmail.com>
2024-08-29 19:55:21 +0200

    loops

---
+++ loops/forLoop.java
@@ -0,0 +1,17 @@
+package loops;
+
+public class forLoop {
+    public static void main(String[] args) {
+        int[][] tab = {{0, 3}, {1, 4}};
+        for(int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
+
+        for(int[] myTab : tab){
+            System.out.println(myTab[0] + " " + myTab[1]);
+        }
+
+
+
+    }
+}
