1cbe8bec2ac70686324274f0c00f48ac3204ac14
DawidSzoka1 <oficer130@gmail.com>
2024-09-02 19:04:17 +0200

    interface class in class and some more objects

---
+++ objectPrograming/ClassInClass.java
@@ -0,0 +1,39 @@
+package objectPrograming;
+
+public class ClassInClass {
+
+    public static void main(String[] args) {
+        A a = new A();
+
+        A.B varibleName =  a.new B();
+        A.C varib = new A.C();
+    }
+}
+
+class A{
+
+    private int test;
+    A(){
+        System.out.println("A");
+    }
+    class B{
+        private int testB;
+        B(){
+            System.out.println("B");
+        }
+        void display(){
+            System.out.println(test);
+        }
+    }
+    static class C{
+        C(){
+            System.out.println("C");
+        }
+    }
+    void dis(){
+//        System.out.println(testB); // can't do that
+        B b = new B();
+        b.display();
+        System.out.println(b.testB);
+    }
+}
---
+++ objectPrograming/InterfacesTest.java
@@ -0,0 +1,68 @@
+package objectPrograming;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class InterfacesTest {
+    public static void main(String[] args) {
+        System.out.println(Name.PI);
+        Name a = new NameImplements();
+//        Name b = new Name();  don't work
+        int[] tab = {2, 4, 1};
+        NameImplements[] b = new NameImplements[4];
+        b[0] = new NameImplements(3);
+        b[1] = new NameImplements(5);
+        b[2] = new NameImplements(6);
+        b[3] = new NameImplements(2);
+        Arrays.sort(tab);
+        System.out.println(NameImplements.PI);
+        System.out.println(Arrays.toString(tab));
+        Arrays.sort(b);
+        System.out.println(Arrays.toString(b));
+        
+
+    }
+}
+
+interface  Name{// can't by public
+    double PI = 3.14;// public static and final
+    void display();// public abstract
+
+}
+interface Surname{
+
+}
+
+class NameImplements implements Name, Surname, Comparable { // In java you can implements more than one interface
+    NameImplements(){}
+    int x;
+    NameImplements(int x){
+        this.x = x;
+    }
+    @Override
+    public void display() {
+        System.out.println("Person");
+    }
+
+    @Override
+    public int compareTo(@NotNull Object o) {
+        if(this.x > ((NameImplements)o).x){
+            return 1;
+        }else if(this.x < ((NameImplements)o).x){
+            return -1;
+        }
+        return 0;
+    }
+    public String toString(){
+        return "Person " + this.x;
+    }
+}
+/*
+can't multiple inheritance
+class S extends t, m{
+
+}
+
+ */
