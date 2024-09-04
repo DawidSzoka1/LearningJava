ea55ba19b3ca23e0bac24e26bb4e012f104aa987
DawidSzoka1 <oficer130@gmail.com>
2024-09-04 15:57:16 +0200

    try catch and object as a field in class

---
+++ objectPrograming/HandlingErrors.java
@@ -0,0 +1,31 @@
+package objectPrograming;
+
+public class HandlingErrors {
+    public static void main(String[] args){
+        int a = 10;
+        try {
+            if (a == 10) {
+                throw new MyExeption("my error");
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        try{
+            System.out.println(5/1);
+        }catch(ArithmeticException e){
+            System.out.println("Cannot divide by zero " + e);
+        }catch(Exception e){
+
+        } finally{
+            System.out.println("finally block");//allways do
+        }
+
+    }
+}
+
+
+class MyExeption extends Exception{
+    public MyExeption(String msg){
+        super(msg);
+    }
+}
