470f747c886563d7b7bdce84e147abb3963da5ee
DawidSzoka1 <oficer130@gmail.com>
2024-09-05 20:54:04 +0200

    some string

--- MathClass.java
+++ MathClass.java
@@ -4,10 +4,12 @@ public class MathClass {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter value x: ");
+
         double x = input.nextDouble();
         System.out.println("Enter value y: ");
         double y = input.nextDouble();
         double z = Math.sqrt(x*x + y*y);
+
 
         System.out.println(z);
         System.out.println(Math.pow(x, y));
---
+++ StringMethods.java
@@ -0,0 +1,14 @@
+public class StringMethods {
+    public static void main(String[] args) {
+        String name = "name";
+        System.out.println(name.equalsIgnoreCase("Name"));
+        System.out.println(name.length());
+        System.out.println(name.charAt(0));
+        for(char c: name.toCharArray()){
+            System.out.println(c);
+        }
+        for(int i = 0; i<name.length(); i++){
+            System.out.println(name.charAt(i));
+        }
+    }
+}
