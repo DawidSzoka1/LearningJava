1150513d2a76598d5e3f39dfa09f4765ca3da426
DawidSzoka1 <oficer130@gmail.com>
2024-09-08 11:50:59 +0200

    multi threading

--- ThreadsJava/MyRunnable.java
+++ ThreadsJava/MyRunnable.java
@@ -1,16 +1,17 @@
 package ThreadsJava;
 
-public class MyThread extends Thread {
+public class MyRunnable implements Runnable {
+
     @Override
     public void run() {
-        for(int i = 10; i > 0; i--){
-            System.out.println("Thread #1 : "+i);
+        for(int i = 0; i < 10; i++){
+            System.out.println("Thread #2 : "+i);
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 System.out.println(e.getMessage());
             }
         }
-        System.out.println("Thread #1 is finished");
+        System.out.println("Thread #2 is finished");
     }
 }
