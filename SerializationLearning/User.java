f8c5a69ff81963dd9e3b65240b566bcd3755a0ab
DawidSzoka1 <oficer130@gmail.com>
2024-10-07 21:07:59 +0200

    user

--- SerializationLearning/User.java
+++ SerializationLearning/User.java
@@ -1,8 +1,11 @@
 package SerializationLearning;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
     String name;
     transient String password;//dont go to serializer
     public void sayHello() {
