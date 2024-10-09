fe6787cba98019391ac15add98d2c13731b903f3
DawidSzoka1 <oficer130@gmail.com>
2024-10-09 09:38:13 +0200

    some work done

--- University/lab02/Point.java
+++ University/lab02/Point.java
@@ -1,5 +1,8 @@
 package University.lab02;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Point{
     private int x, y;
 
@@ -27,10 +30,7 @@ public class Point{
 
     @Override
     public String toString() {
-        return "Point{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
+        return "(" + this.getX() + "," + this.getY() +  ')';
     }
     void wypisz(){
         System.out.println("("+ this.getX() + "," + this.getY() + ")");
@@ -38,6 +38,22 @@ public class Point{
     public static void main(String[] args) {
         Point p1 = new Point(1, 2);
         p1.wypisz();
+        System.out.println(p1);
+        Point[] points = new Point[10];
+        Random rand = new Random();
+        for(int i = 0; i < points.length; i++){
+            points[i] = new Point(rand.nextInt(2), rand.nextInt(2));
+//            System.out.println(points[i]);
+        }
+//        System.out.println(Arrays.toString(points));
+
+        Odcinek[] o = new Odcinek[10];
+        for(int i = 0; i < o.length; i++){
+            o[i] = new Odcinek(points[i], points[rand.nextInt(points.length)]);
+        }
+        Odcinek[] max = Odcinek.maxLenght(o);
+        System.out.println(Arrays.toString(max));
+
     }
 }
 
