e02b849d162b9b31c591193017a2eeefc88a5f41
DawidSzoka1 <oficer130@gmail.com>
2024-10-30 09:30:59 +0100

    fixing some errors

--- University/lab04/Car.java
+++ University/lab04/Car.java
@@ -3,24 +3,15 @@ package University.lab04;
 public class Car {
     private String make;
     private int year;
-    private String type;
     public Car(String make, int year) {
         this.make = make;
         this.year = year;
-    }
-    public Car(String make, int year, String type) {
-        this.make = make;
-        this.year = year;
-        this.type = type;
     }
     public Car(){}
 
     @Override
     public String toString() {
-        return "Car{" +
-                "make='" + make + '\'' +
-                ", year=" + year +
-                ", type='" + type + '\'' +
-                '}';
+        return  make  +
+                " " + year;
     }
 }
--- University/lab04/Gasoline.java
+++ University/lab04/Gasoline.java
@@ -4,10 +4,15 @@ public class Gasoline extends Car{
     private String type;
 
     public Gasoline(String make, int year, String type) {
-        super(make, year, type);
+        super(make, year);
+        this.type = type;
     }
 
     public Gasoline() {
     }
 
+    @Override
+    public String toString() {
+        return super.toString() + ", " + type;
+    }
 }
