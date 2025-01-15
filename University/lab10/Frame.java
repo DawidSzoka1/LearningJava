40809087899fae44b56c04a9b0d61bd957778205
DawidSzoka1 <oficer130@gmail.com>
2025-01-15 09:52:11 +0100

    radio button

--- University/lab10/Frame.java
+++ University/lab10/Frame.java
@@ -8,39 +8,56 @@ public class Frame extends JFrame {
     JTextField text = new JTextField(20);
     JButton button = new JButton("OK");
     JLabel label = new JLabel();
+    ButtonGroup radioButton = new ButtonGroup();
+    JRadioButton pl = new JRadioButton("PL");
+    JRadioButton eng  = new JRadioButton("ENg");
     public Frame() {
         super("Frame");
         setSize(900, 500);
         setLayout(new FlowLayout());
         add(new JLabel("Podaj imie:"));
         add(text);
-        button.addActionListener(_ -> label.setText("Witaj " + text.getText()));
-        text.addMouseListener(new MouseListener() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                text.setText("");
+
+        pl.setSelected(true);
+        radioButton.add(pl);
+        radioButton.add(eng);
+        add(pl);
+        add(eng);
+        button.addActionListener(_ ->{
+            if(pl.isSelected()){
+                label.setText("Witaj " + text.getText());
+            }else if(eng.isSelected()){
+                label.setText("hello " + text.getText());
+            }else{
+                label.setText("Nic " + text.getText());
             }
-
-            @Override
-            public void mousePressed(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                text.setToolTipText("Tu daj imie cweluszku ");
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-                label.setText("Wracaj do mnie");
-            }
-        });
+        } );
+//        text.addMouseListener(new MouseListener() {
+//            @Override
+//            public void mouseClicked(MouseEvent e) {
+//                text.setText("");
+//            }
+//
+//            @Override
+//            public void mousePressed(MouseEvent e) {
+//
+//            }
+//
+//            @Override
+//            public void mouseReleased(MouseEvent e) {
+//
+//            }
+//
+//            @Override
+//            public void mouseEntered(MouseEvent e) {
+//                text.setToolTipText("Tu daj imie cweluszku ");
+//            }
+//
+//            @Override
+//            public void mouseExited(MouseEvent e) {
+//                label.setText("Wracaj do mnie");
+//            }
+//        });
         add(button);
         add(label);
 
--- University/lab10/Main.java
+++ University/lab10/Main.java
@@ -2,6 +2,6 @@ package University.lab10;
 
 public class Main {
     public static void main(String[] args) {
-        FrameSum frame = new FrameSum();
+        Frame frame = new Frame();
     }
 }
